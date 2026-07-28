c7a9e2b07ea813ebc89809b6a758b6b9b8184469
Joey D. Smith <84807702+joey-io@users.noreply.github.com>
2026-07-27 23:38:32 -0400

    Register all built-in satellite adapters

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import { Obsat } from "./obsat.js";
 import { landsat } from "./adapters/landsat.js";
+import { sentinel1 } from "./adapters/sentinel1.js";
+import { sentinel1Rtc } from "./adapters/sentinel1rtc.js";
 import { sentinel2 } from "./adapters/sentinel2.js";
+import { hlsLandsat } from "./adapters/hlsLandsat.js";
+import { hlsSentinel } from "./adapters/hlsSentinel.js";
 
 export { Obsat } from "./obsat.js";
 export { AdapterRegistry } from "./registry.js";
 export { createStacAdapter } from "./adapters/stac.js";
 export { landsat } from "./adapters/landsat.js";
+export { sentinel1 } from "./adapters/sentinel1.js";
+export { sentinel1Rtc } from "./adapters/sentinel1rtc.js";
 export { sentinel2 } from "./adapters/sentinel2.js";
+export { hlsLandsat } from "./adapters/hlsLandsat.js";
+export { hlsSentinel } from "./adapters/hlsSentinel.js";
 
-export const obsat = new Obsat({ adapters: [landsat, sentinel2] });
+export const adapters = [
+  sentinel1,
+  sentinel1Rtc,
+  sentinel2,
+  landsat,
+  hlsLandsat,
+  hlsSentinel
+];
+
+export const obsat = new Obsat({ adapters });
