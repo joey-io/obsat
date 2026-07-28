d9f554dfa384c614c57223db2373a62b025b9e21
Joey D. Smith <84807702+joey-io@users.noreply.github.com>
2026-07-27 23:47:11 -0400

    Register global air and map adapters

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { earthquakes } from "./adapters/earthquakes.js";
 import { elevation } from "./adapters/elevation.js";
 import { weather } from "./adapters/weather.js";
 import { openMeteo } from "./adapters/openMeteo.js";
+import { openMeteoAir } from "./adapters/openMeteoAir.js";
+import { openStreetMap } from "./adapters/openStreetMap.js";
 import { water } from "./adapters/water.js";
 import { airNow } from "./adapters/airnow.js";
 
@@ -25,9 +27,26 @@ export { earthquakes } from "./adapters/earthquakes.js";
 export { elevation } from "./adapters/elevation.js";
 export { weather } from "./adapters/weather.js";
 export { openMeteo } from "./adapters/openMeteo.js";
+export { openMeteoAir } from "./adapters/openMeteoAir.js";
+export { openStreetMap } from "./adapters/openStreetMap.js";
 export { water } from "./adapters/water.js";
 export { airNow } from "./adapters/airnow.js";
 
-export const adapters = [sentinel1, sentinel1Rtc, sentinel2, landsat, hlsLandsat, hlsSentinel, earthquakes, elevation, openMeteo, weather, water, airNow];
+export const adapters = [
+  sentinel1,
+  sentinel1Rtc,
+  sentinel2,
+  landsat,
+  hlsLandsat,
+  hlsSentinel,
+  earthquakes,
+  elevation,
+  openMeteo,
+  openMeteoAir,
+  openStreetMap,
+  weather,
+  water,
+  airNow
+];
 
 export const obsat = new Obsat({ adapters });
